560c9f7fe472bc12c346b8d5dd8933e8646858bc
jefo3 <jeferson.goncalves.noronha@gmail.com>
2023-11-23 00:29:03 -0300

    feat: creating specification

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,22 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private specificationRepository: ISpecificationRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const specificationAlreadyExist =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExist) {
+      throw new Error("Especificação ja existe");
+    }
+    this.specificationRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationService };
---
+++ src/routes/specification.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+import { SpecificationRepository } from "../modules/cars/repositories/SpecificationRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationRoutes = Router();
+
+const specificationRepository = new SpecificationRepository();
+
+specificationRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationRepository,
+  );
+
+  createSpecificationService.execute({ description, name });
+
+  return response
+    .status(201)
+    .json({ message: "Especificaçao criada com sucesso" });
+});
+
+export { specificationRoutes };
