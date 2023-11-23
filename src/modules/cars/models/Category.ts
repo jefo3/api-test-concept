41d98953d1d98cfe206d25599f210d5b0e676883
jefo3 <jeferson.goncalves.noronha@gmail.com>
2023-11-23 00:31:37 -0300

    fix: Simplifying Category Model Class

--- src/modules/cars/models/Category.ts
+++ src/modules/cars/models/Category.ts
@@ -1,51 +1,15 @@
 import { v4 as uuidV4 } from "uuid";
 
 class Category {
-  private id?: string;
-  private name: string;
-  private description: string;
-  private created_at: string;
+  id?: string;
+  name: string;
+  description: string;
+  created_at: string;
 
-  constructor(name?: string, description?: string, created_at?: string) {
+  constructor() {
     if (!this.id) {
       this.id = uuidV4();
     }
-
-    if (name) this.name = name;
-    if (description) this.description = description;
-    if (created_at) this.created_at = created_at;
-  }
-
-  public setId(id: string) {
-    this.id = id;
-  }
-
-  public getId(): string {
-    return this.id;
-  }
-
-  public setName(name: string) {
-    this.name = name;
-  }
-
-  public getName(): string {
-    return this.name;
-  }
-
-  public setDescription(description: string) {
-    this.description = description;
-  }
-
-  public getDescription(): string {
-    return this.description;
-  }
-
-  public setCreatedAt(created_at: string) {
-    this.created_at = created_at;
-  }
-
-  public getCreatedAt(): string {
-    return this.created_at;
   }
 }
 
