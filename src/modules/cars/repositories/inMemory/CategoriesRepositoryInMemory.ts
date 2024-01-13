5204c665d30ad3d0e0f53e82612cf6d57a1d1a13
jefo3 <jeferson.goncalves.noronha@gmail.com>
2024-01-13 11:54:07 -0300

    feat: creating implemention in memory of category

---
+++ src/modules/cars/repositories/inMemory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,35 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+
+  async listAll(): Promise<Category[]> {
+    return this.categories;
+  }
+
+  async create({ description, name }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepositoryInMemory };
